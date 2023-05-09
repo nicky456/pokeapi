5e74f1a609040f0c72a9ff337034244f92afd725
Nikolett Halasi-Szabó <nikolett.halasi-szabo@skvad.com>
2023-05-09 14:00:04 +0200

    .

--- src/components/Checkbox.tsx
+++ src/components/Checkbox.tsx
@@ -16,7 +16,7 @@ const Checkbox: React.FC<CheckboxProps> = ({ checked, setChecked }) => {
             <LabelIconUncheckedWrapper>
               <svg
                 width="20"
-                height="20"
+                height="20 "
                 viewBox="0 0 20 20"
                 fill="none"
                 xmlns="http://www.w3.org/2000/svg"
